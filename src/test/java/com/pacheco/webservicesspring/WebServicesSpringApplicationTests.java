b467ae7a549f9b0a8bc0996897bfe724813a1c20
Lucas Pacheco <lucas.vinicius719@gmail.com>
2020-10-21 16:14:42 -0300

    Project Created

---
+++ src/test/java/com/pacheco/webservicesspring/WebServicesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pacheco.webservicesspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
