a2bfe56e18a7dbcad9fca69c8c4a038be78a9dd3
Lucas Pacheco <lucas.vinicius719@gmail.com>
2020-10-22 11:47:55 -0300

    User services

---
+++ src/main/java/com/pacheco/webservicesspring/services/UserService.java
@@ -0,0 +1,27 @@
+package com.pacheco.webservicesspring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pacheco.webservicesspring.entities.User;
+import com.pacheco.webservicesspring.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
