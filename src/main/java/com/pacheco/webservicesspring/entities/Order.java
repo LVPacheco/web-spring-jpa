763ec3cfad0e749e9e54434415957bbc8d6578a2
Lucas Pacheco <lucas.vinicius719@gmail.com>
2020-10-22 13:54:26 -0300

    OrderStatus enum

--- src/main/java/com/pacheco/webservicesspring/entities/Order.java
+++ src/main/java/com/pacheco/webservicesspring/entities/Order.java
@@ -12,6 +12,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.pacheco.webservicesspring.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order")
@@ -25,6 +26,9 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 
+	private Integer orderStatus;
+	
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -33,10 +37,10 @@ public class Order implements Serializable {
 
 	}
 
-	public Order(Long id, Instant moment, User client) {
-		super();
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -61,6 +65,24 @@ public class Order implements Serializable {
 	}
 
 	public void setUser(User client) {
+		this.client = client;
+	}
+	
+	
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus!=null) this.orderStatus = orderStatus.getCode();
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
 		this.client = client;
 	}
 
