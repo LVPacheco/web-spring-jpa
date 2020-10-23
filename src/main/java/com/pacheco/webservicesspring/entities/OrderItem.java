4ea4d48aac19c5bc8e324c3b49d628f717bf2389
Lucas Pacheco <lucas.vinicius719@gmail.com>
2020-10-23 12:19:06 -0300

    Payment

--- src/main/java/com/pacheco/webservicesspring/entities/OrderItem.java
+++ src/main/java/com/pacheco/webservicesspring/entities/OrderItem.java
@@ -13,10 +13,10 @@ import com.pacheco.webservicesspring.entities.PK.OrderItemPK;
 @Table(name = "tb_order_item")
 public class OrderItem implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@EmbeddedId
 	private OrderItemPK id = new OrderItemPK();
-	
+
 	private Integer quantity;
 	private Double price;
 
@@ -30,20 +30,20 @@ public class OrderItem implements Serializable {
 		this.quantity = quantity;
 		this.price = price;
 	}
-	
+
 	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
-	
+
 	public void setOrder(Order order) {
 		id.setOrder(order);
 	}
-	
+
 	public Product getProduct() {
 		return id.getProduct();
 	}
-	
+
 	public void setProduct(Product product) {
 		id.setProduct(product);
 	}
